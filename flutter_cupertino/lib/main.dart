02fc30485a6b898c4b4ed36e345ed5cf5abcdf05
hakonamatata <jan@sviland.org>
2022-03-12 18:50:49 +0100

    flutter ios app, basic navigation

--- flutter_cupertino/lib/main.dart
+++ flutter_cupertino/lib/main.dart
@@ -1,5 +1,5 @@
+import 'dart:developer';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,13 @@ class MyApp extends StatelessWidget {
     return CupertinoApp(
       title: 'Flutter Demo',
       // theme: CupertinoThemeData(
-      //   primaryColor: CupertinoColors.activeBlue,
+      //   textTheme: CupertinoTextThemeData(
+      //     navLargeTitleTextStyle: TextStyle(
+      //       fontWeight: FontWeight.bold,
+      //       fontSize: 70.0,
+      //       color: CupertinoColors.white,
+      //     ),
+      //   ),
       // ),
       home: MyHomePage(),
     );
@@ -24,12 +30,69 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-        child: Center(
-      child: Text(
-        'Hello, World!',
-        style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle,
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.book_solid), label: 'Articles'),
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.eye_solid), label: 'Views'),
+        ],
       ),
-    ));
+      tabBuilder: (context, i) {
+        return CupertinoTabView(
+          builder: (context) {
+            return CupertinoPageScaffold(
+              navigationBar: CupertinoNavigationBar(
+                middle: i == 0 ? Text('Articles') : Text('Views'),
+              ),
+              child: Center(
+                child: CupertinoButton(
+                  child: Text('This is tab $i', style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      CupertinoPageRoute(builder: (context) {
+                        print('pressed $i');
+                        return DetailPage(i == 0 ? 'Articles' : 'Views');
+                      }),
+                    );
+                  },
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+
+    // Hello world
+    // return CupertinoPageScaffold(
+    //   child: Center(
+    //     child: Text(
+    //       'Hello, World!',
+    //       style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle,
+    //     ),
+    //   )
+    // );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  // const DetailPage({Key? key}) : super(key: key);
+
+  final String topic;
+  const DetailPage(this.topic);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Detail'),
+      ),
+      child: Center(
+        child: Text(
+          'This is detail page for $topic',
+          style: CupertinoTheme.of(context).textTheme.navLargeTitleTextStyle,
+        ),
+      ),
+    );
   }
 }
