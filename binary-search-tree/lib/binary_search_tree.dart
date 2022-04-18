66335e195bb50edd4d393eb86a1e86e89efd3986
hakonamatata <jan@sviland.org>
2022-04-18 11:54:38 +0200

    binary search tree

---
+++ binary-search-tree/lib/binary_search_tree.dart
@@ -0,0 +1,87 @@
+class BinarySearchTree {
+  Node? root;
+  var _sortedData = <String>[];
+
+  List<String> get sortedData {
+    sort();
+    return _sortedData;
+  }
+
+  BinarySearchTree(String s) {
+    insert(s);
+  }
+
+  void sort() {
+    _sortedData.clear();
+    inOrderTraversal(root!);
+  }
+
+  void inOrderTraversal(Node? node) {
+    if (node != null) {
+      inOrderTraversal(node.left);
+      _sortedData.add(node.value.toString());
+      inOrderTraversal(node.right);
+    }
+  }
+
+  void insert(String s) {
+    for (int i = 0; i < s.length; i++) {
+      insertSingleNumber(int.parse(s[i]));
+    }
+  }
+
+  void insertSingleNumber(int value) {
+    Node? y = null;
+    var node = root;
+
+    while (node != null) {
+      y = node;
+
+      if (value <= node.value) {
+        node = node.left;
+      } else {
+        node = node.right;
+      }
+    }
+
+    if (y == null) {
+      root = Node(value);
+    } else if (value <= y.value) {
+      y.left = Node(value);
+    } else {
+      y.right = Node(value);
+    }
+  }
+}
+
+class Node {
+  int value;
+  Node? left;
+  Node? right;
+  Node? parent;
+
+  String get data {
+    return value.toString();
+  }
+
+  Node(this.value) {}
+
+  @override
+  String toString() {
+    return "value: $value, children: [$left, $right]";
+  }
+}
+
+void main() {
+  BinarySearchTree bst = BinarySearchTree('4');
+  bst.insert('2');
+  bst.insert('6');
+  bst.insert('1');
+  bst.insert('3');
+  bst.insert('5');
+  bst.insert('7');
+
+  print(bst.sortedData);
+
+  print(bst.root);
+}
---
+++ binary-search-tree/lib/binary_search_tree_2.dart
@@ -0,0 +1,116 @@
+import 'dart:math';
+
+class BinarySearchTree2 {
+  Node? root;
+  var _sortedData = <num>[];
+
+  List<num> get sortedData {
+    sort();
+    return _sortedData;
+  }
+
+  BinarySearchTree2(List<num> array) {
+    insertList(array);
+  }
+
+  void sort() {
+    _sortedData.clear();
+    inOrderTraversal(root!);
+  }
+
+  List<String> printVisualTree() {
+    var rows = <String>[];
+
+    rows.add('       ${root?.value}');
+    rows.add('     /   \\');
+    rows.add('    /     \\');
+    rows.add('   ${root?.left?.value}       ${root?.right?.value}');
+
+    var buffer = StringBuffer();
+    buffer.writeAll(rows, '\n');
+
+    print(buffer.toString());
+
+    return rows;
+    // for (var i = 0; i < levels; i++) {}
+
+    // TODO: print like this:
+    //       4
+    //     /   \
+    //    /     \
+    //   2       6
+    //  / \     / \
+    // 1   3   5   7
+  }
+
+  void inOrderTraversal(Node? node) {
+    if (node != null) {
+      inOrderTraversal(node.left);
+      _sortedData.add(node.value);
+      inOrderTraversal(node.right);
+    }
+  }
+
+  void insertList(List<num> list) {
+    for (int i = 0; i < list.length; i++) {
+      insert(list[i]);
+    }
+  }
+
+  void insert(num value) {
+    Node? y = null;
+    var node = root;
+
+    while (node != null) {
+      y = node;
+
+      if (value <= node.value) {
+        node = node.left;
+      } else {
+        node = node.right;
+      }
+    }
+
+    if (y == null) {
+      root = Node(value);
+    } else if (value <= y.value) {
+      y.left = Node(value);
+    } else {
+      y.right = Node(value);
+    }
+  }
+}
+
+class Node {
+  num value;
+  Node? left;
+  Node? right;
+  Node? parent;
+
+  Node(this.value) {}
+
+  @override
+  String toString() {
+    return "value: $value, children: [$left, $right]";
+  }
+}
+
+void main() {
+  BinarySearchTree2 bst = BinarySearchTree2([4]);
+  bst.insert(2.687564);
+  bst.insert(6.234);
+  bst.insert(1.8435);
+  bst.insert(3.123);
+  bst.insert(5.678);
+  bst.insert(7.123);
+
+  for (var i = 0; i < 100000; i++) {
+    bst.insert(Random().nextDouble());
+  }
+
+  // bst.printVisualTree();
+
+  print(bst.sortedData);
+
+  // print(bst.root);
+}
